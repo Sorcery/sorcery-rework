450e6f576845cce6ef93ca069f4ffbe83faf8dbb
Josh Buker <crypto@joshbuker.com>
2020-08-04 23:06:53 -0700

    Add note to either create specs or delete file for config_spec

--- spec/sorcery/config_spec.rb
+++ spec/sorcery/config_spec.rb
@@ -3,6 +3,8 @@
 require 'spec_helper'
 require 'sorcery-core'
 
+# TODO: Add specs or delete file.
+# rubocop:disable RSpec/EmptyExampleGroup
 RSpec.describe Sorcery::Config do
-  # TODO: Config specs
 end
+# rubocop:enable RSpec/EmptyExampleGroup
