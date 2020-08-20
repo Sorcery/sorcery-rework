d4bdf00845859bee65ce153ff1a513ae05c16978
Josh Buker <crypto@joshbuker.com>
2020-08-20 10:55:41 -0700

    More good the English

--- sorcery-core/lib/sorcery/controller.rb
+++ sorcery-core/lib/sorcery/controller.rb
@@ -255,9 +255,9 @@ module Sorcery
       # returns nil instead.
       #
       # Important to note, `find` does not return the result of the block, but
-      # instead the element. In this case, it would return the symbol of the
-      # login source method rather than the return value from the block. That's
-      # why we must use the result placeholder variable.
+      # instead the element found. In this case, it would return the symbol of
+      # the login source method rather than the return value of that method.
+      # That's why we must use the result placeholder variable.
       #
       def login_from_other_sources
         result = nil
