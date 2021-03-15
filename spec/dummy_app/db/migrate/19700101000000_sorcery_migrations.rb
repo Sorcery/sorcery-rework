549e694b8358d047c584c075ff09588f37377c0b
Josh Buker <crypto@joshbuker.com>
2021-03-15 11:22:43 -0700

    Remove activity logging attributes from admin - unused

--- spec/dummy_app/db/migrate/19700101000000_sorcery_migrations.rb
+++ spec/dummy_app/db/migrate/19700101000000_sorcery_migrations.rb
@@ -8,12 +8,6 @@ class SorceryMigrations < ActiveRecord::Migration[6.0]
       t.string :crypted_password
       t.string :salt
 
-      # Activity Logging
-      t.datetime :last_login_at,              default: nil
-      t.datetime :last_logout_at,             default: nil
-      t.datetime :last_activity_at,           default: nil
-      t.string   :last_login_from_ip_address, default: nil
-
       # Brute Force Protection
       t.integer  :failed_logins_count, default: 0
       t.datetime :lock_expires_at,     default: nil
