c586997ab5c84c2f3e271b43770b6f35d6620807
Josh Buker <crypto@joshbuker.com>
2023-03-04 18:54:09 -0800

    Update load_defaults to suppress deprecation warnings

--- spec/dummy_app/config/application.rb
+++ spec/dummy_app/config/application.rb
@@ -4,7 +4,7 @@ require File.expand_path('boot', __dir__)
 
 module DummyApp
   class Application < Rails::Application
-    config.load_defaults '6.0'
+    config.load_defaults '6.1'
 
     config.eager_load = false
     config.action_controller.allow_forgery_protection = false
