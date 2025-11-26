6a8826bdb641ddba11208bc0c5157eb63b675144
Josh Buker <crypto@joshbuker.com>
2025-11-26 12:34:10 -0800

    This was sitting in my stash, saving it for later

--- spec/sorcery/plugins/activity_logging/controller_spec.rb
+++ spec/sorcery/plugins/activity_logging/controller_spec.rb
@@ -28,4 +28,50 @@ RSpec.describe Sorcery::Plugins::ActivityLogging::Controller do
     end
     # rubocop:enable RSpec/ExampleLength
   end
+
+  describe 'instance method' do
+    let(:controller_class) do
+      controller_class = Class.new(ActionController::Base)
+
+      controller_class.authenticates_with_sorcery! do |config|
+        config.load_plugin(:activity_logging, controller: test_config)
+      end
+
+      controller_class
+    end
+
+    subject(:controller_instance) { controller_class.new }
+
+    let(:test_config) { {} }
+    let(:username) { Faker::Internet.username }
+    let(:password) { Faker::Internet.password }
+    # TODO: Should this use a stub instead of the dummy app factory?
+    let!(:user) { create(:user, username: username, password: password) }
+
+    describe 'login' do
+      context 'when register_login_time is true' do
+        let(:test_config) { { register_login_time: true } }
+
+        it 'logs login time' do
+          expect(user.last_login_at).to be_nil
+
+          controller_instance.login(username, password)
+
+          expect(user.last_login_at).to be_present
+        end
+      end
+
+      context 'when register_login_time is false' do
+        let(:test_config) { { register_login_time: false } }
+
+        it 'does not log login time' do
+          expect(user.last_login_at).to be_nil
+
+          controller_instance.login(username, password)
+
+          expect(user.last_login_at).to be_nil
+        end
+      end
+    end
+  end
 end
